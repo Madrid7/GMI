f07099463fbd9b105bb50c2ac1f0c900353e72cf
Ricky7 <black.id64@gmail.com>
2014-06-25 22:26:17 +0700

    update profile worked

--- UserBundle/Controller/DefaultController.php
+++ UserBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace GMI\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use GMI\UserBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -31,8 +33,30 @@ class DefaultController extends Controller
         return $this->render('GMIUserBundle:User:profil.html.twig');
     }
 
-    public function ubahprofilAction()
+    public function ubahprofilAction(Request $request)
     {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if($request->getMethod()=='POST'){
+            
+            $userManager = $this->get('fos_user.user_manager');
+            $name = $request->get('name');
+            $sex = $request->get('sex');
+            $address = $request->get('address');
+            $town = $request->get('town');
+            $cp = $request->get('cp');
+            
+            $user->setName($name);
+            $user->setSex($sex);
+            $user->setAddress($address);
+            $user->setTown($town);
+            $user->setCp($cp);
+            $userManager->updateUser($user);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirect($this->generateUrl('profil_page'));
+        }
+
         return $this->render('GMIUserBundle:User:ubahprofil.html.twig');
     }
 }
